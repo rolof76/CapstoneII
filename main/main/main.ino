8a910257c5687a5e6dca48a515131e338dbee71c
rolof76 <rohloffben@gmail.com>
2018-03-27 11:13:59 -0500

    main

---
+++ main/main/main.ino
@@ -0,0 +1,24 @@
+#include "MIDIUSB.h"
+
+void noteOn(byte channel, byte pitch, byte velocity){
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  noteOn(1,48,127);
+  delay(1000);
+  noteOff(1,48,0);
+  delay(0);
+}
