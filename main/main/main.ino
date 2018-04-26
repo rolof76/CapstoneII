902516df31e7358a6d8d7204a7561906c6d23477
rolof76 <rohloffben@gmail.com>
2018-04-26 10:47:13 -0500

    working prototype
    works with little hiccups

--- main/main/main.ino
+++ main/main/main.ino
@@ -10,19 +10,44 @@ void noteOff(byte channel, byte pitch, byte velocity) {
   MidiUSB.sendMIDI(noteOff);
 }
 
+const int TRIG_PIN = 1; //pd3
+const int ECHO_PIN = 4; //pd4
+int cm_new = 20;
+int cm_old = 20;
+float cm;
+long duration;
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  //Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("noteOn");
-  noteOn(1,48,127);
-  MidiUSB.flush();
-  delay(1000);
-  Serial.println("noteOff");
-  noteOff(1,48,0);
-  MidiUSB.flush();
-  delay(1000);
+
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  cm_new = map(constrain(duration, 20, 10000), 6000, 50, 96, 24);
+
+  if((cm_new > 96)||(cm_new <  24)) {
+    noteOff(0, cm_old, 0);
+    cm_old = cm_new;
+  }
+  else if(cm_new != cm_old) {
+    noteOn(0, cm_new, 127);
+    MidiUSB.flush();
+    delay(15);
+    noteOff(0, cm_old, 0);
+    MidiUSB.flush();
+    cm_old = cm_new;
+  }
+  delay(15);
 }
